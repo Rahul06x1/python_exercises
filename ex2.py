be7ae98583a2de6379958d2b87b71951f75c776a
Rahul Rajeev <rahulrajeev2k1@gmail.com>
2023-11-01 15:53:23 +0530

    Altered function evaluate [ Green: Substraction of two operands ]

--- ex2.py
+++ ex2.py
@@ -1,10 +1,13 @@
 def evaluate(n):
     stack = []
     for i in n:
-        if i in ['+']:
+        if i in ['+', '-']:
             operand_1 = stack.pop()
             operand_2 = stack.pop()
-            stack.append(operand_1 + operand_2)
+            if i == '+':
+                stack.append(operand_1 + operand_2)
+            elif i == '-':
+                stack.append(operand_2 - operand_1)
         else:
             stack.append(int(i))
 
