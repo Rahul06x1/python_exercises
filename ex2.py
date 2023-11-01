bf7a5377a7dc4a41c020c3a5fd59b008104922b5
Rahul Rajeev <rahulrajeev2k1@gmail.com>
2023-11-01 16:51:27 +0530

    Alter function evaluate [ Green: Exclusion of null input ]

--- ex2.py
+++ ex2.py
@@ -1,21 +1,23 @@
 import string
 def evaluate(n):
     stack = []
-    for i in n:
-        if i not in '0123456789+-/*':
-            return False
-        if i in ['+', '-', '/', '*']:
-            operand_1 = stack.pop()
-            operand_2 = stack.pop()
-            if i == '+':
-                stack.append(operand_1 + operand_2)
-            elif i == '-':
-                stack.append(operand_2 - operand_1)
-            elif i == '/':
-                stack.append(operand_2 // operand_1)
-            elif i == '*':
-                stack.append(operand_1 * operand_2)
-        else:
-            stack.append(int(i))
+    if n != '':
+        for i in n:
+            if i not in '0123456789+-/*':
+                return False
+            if i in ['+', '-', '/', '*']:
+                operand_1 = stack.pop()
+                operand_2 = stack.pop()
+                if i == '+':
+                    stack.append(operand_1 + operand_2)
+                elif i == '-':
+                    stack.append(operand_2 - operand_1)
+                elif i == '/':
+                    stack.append(operand_2 // operand_1)
+                elif i == '*':
+                    stack.append(operand_1 * operand_2)
+            else:
+                stack.append(int(i))
 
-    return stack.pop()
+        return stack.pop()
+    return False
