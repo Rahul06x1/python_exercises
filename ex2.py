67b7375db8be68647d2cd8c06d6c62c4e4e4cc72
Rahul Rajeev <rahulrajeev2k1@gmail.com>
2023-11-01 15:40:07 +0530

    Altered function evaluate [ Green: Addition of two operands ]

--- ex2.py
+++ ex2.py
@@ -1,2 +1,11 @@
 def evaluate(n):
-    return int(n)
+    stack = []
+    for i in n:
+        if i in ['+']:
+            operand_1 = stack.pop()
+            operand_2 = stack.pop()
+            stack.append(operand_1 + operand_2)
+        else:
+            stack.append(int(i))
+
+    return stack.pop()
