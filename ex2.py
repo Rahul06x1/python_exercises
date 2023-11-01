644edbc448bcc23927d27607429bfd7ef1a7cb8f
Rahul Rajeev <rahulrajeev2k1@gmail.com>
2023-11-01 16:03:07 +0530

    Alter function evaluate [ Green: Division of two operands ]

--- ex2.py
+++ ex2.py
@@ -1,13 +1,15 @@
 def evaluate(n):
     stack = []
     for i in n:
-        if i in ['+', '-']:
+        if i in ['+', '-', '/']:
             operand_1 = stack.pop()
             operand_2 = stack.pop()
             if i == '+':
                 stack.append(operand_1 + operand_2)
             elif i == '-':
                 stack.append(operand_2 - operand_1)
+            elif i == '/':
+                stack.append(operand_2 // operand_1)
         else:
             stack.append(int(i))
 
